5e354672a93c5d9439234a4493fe77397445dbea
wlotherington <will@lotherington.me>
2026-08-04 00:04:02 -0700

    Make lotherington.me the canonical domain, redirect legacy domains

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wlotherington.com",
+  site: "https://lotherington.me",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
