6485e4d0c32a1c692ee1d839fbfcbd8ca07feaeb
wlotherington <will@lotherington.me>
2026-08-03 17:41:49 -0700

    Set production site URL and add Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://projects-and-thoughts.netlify.app",
   integrations: [preact()]
 });
