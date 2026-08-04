a2c59b148e9b429e2f1c0a88867be16aaa7f8d41
wlotherington <will@lotherington.me>
2026-08-03 23:37:40 -0700

    Point site URL at wlotherington.com for domain cutover

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wlotherington.netlify.app",
+  site: "https://wlotherington.com",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
